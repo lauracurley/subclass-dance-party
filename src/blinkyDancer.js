c68022d3dc8726cca8d2cdfa045f7c02d04ec723
lauracurley <info@lauracurley.com>
2016-03-08 16:30:25 -0800

    Added local invocation of set position

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,6 +2,8 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="blinkyDancer"></span>');
+  this.setPosition(top, left);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);

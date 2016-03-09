d1c3e73eea18f5d0925a9a151f90a0b2896ff958
lauracurley <info@lauracurley.com>
2016-03-08 16:36:04 -0800

    Create a napoloeon sublclass

---
+++ src/napoleon.js
@@ -0,0 +1,17 @@
+var makeNapoleonDancer = function(top, left, timeBetweenSteps) {
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="napoleon"><img class="test" src="napoleon.gif"></span>');
+  this.setPosition(top, left);
+};
+
+makeNapoleonDancer.prototype = Object.create(makeDancer.prototype);
+makeNapoleonDancer.prototype.constructor = makeNapoleonDancer;
+makeNapoleonDancer.prototype.oldStep = makeDancer.prototype.step; //OMG!!!!
+
+makeNapoleonDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  //this.$node.toggle();
+};
